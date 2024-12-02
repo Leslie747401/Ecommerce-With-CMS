948d4118db5979b2292254ea288976e66dd3add2
Leslie747401 <leslie747401@gmail.com>
2024-12-03 02:06:46 +0530

    Resolved Deployment issues

--- app/(pages)/Cart/page.tsx
+++ app/(pages)/Cart/page.tsx
@@ -1,8 +1,5 @@
-import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import PromotionalBanner from '@/components/PromotionalBanner'
-import TermsAndConditions from '@/components/TermsAndConditions'
-import React from 'react'
 
 export default function Page() {
   return (
